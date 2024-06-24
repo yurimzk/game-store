71bacbfdce6dd3d0ccf9a1e4ecebdd8ed5fa9c7b
Yuri Tagomori <goncalvessyuri@gmail.com>
2024-06-24 17:01:03 -0300

    add index, show, new and create methods to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,34 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: %i[show edit update destroy]
+
   def index
-    @games = policy_scope(Game)
+    @games = Game.all
+  end
+
+  def show
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(params[:game])
+
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:title, :description, :genre, :release_date, :developer, :publiser, :price, :cover_image, :trailer_url)
   end
 end
